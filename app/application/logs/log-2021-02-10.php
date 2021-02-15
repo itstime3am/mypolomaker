973497ce6539d8286bfd20f57aa15a9f686d484b
teerawat <teerawat.kruelit@gmail.com>
2021-02-15 18:20:36 +0700

    update : column add รหัสินค้า

---
+++ app/application/logs/log-2021-02-10.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-02-10 11:37:35 --> Severity: Warning --> Use of undefined constant DEFAULT_TITLE - assumed 'DEFAULT_TITLE' (this will throw an Error in a future version of PHP) C:\wamp64\www\public_html\app\application\views\_public\_template_main.php 5
+ERROR - 2021-02-10 11:38:26 --> Severity: Warning --> Use of undefined constant DEFAULT_TITLE - assumed 'DEFAULT_TITLE' (this will throw an Error in a future version of PHP) C:\wamp64\www\public_html\app\application\views\_public\_template_main.php 5
